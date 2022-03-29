6dba8e027eda88614070699970ffbc87216ba4a1
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-03-29 17:59:35 +0600

    update

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Posts = () => {
+    const [posts, setPosts] = useState({})
+
+    useEffect(() => {
+        fetch('http://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => setPosts(data))
+    },[])
     return (
         <div>
-            <h2>Here is all facebook posts</h2>
+            <h2>Here is all facebook posts:{posts.length}</h2>
+            {
+               
+            }
         </div>
     );
 };
